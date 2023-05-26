6ebfc7222fa9e9ba4a99d7536d56996e7946d613
augustin.giraudier <augustin.giraudier@etu.uca.fr>
2023-05-26 15:28:37 +0200

    UI sliders

--- App/App/Views/UEView.swift
+++ App/App/Views/UEView.swift
@@ -6,9 +6,27 @@
 //
 
 import SwiftUI
+import Model
 
 struct UEView: View {
+    
+    @State private var value : CGFloat = 7
+    @State private var pixelLen : CGFloat = 5.0
+    @State private var barColor : Color = .red
+    @State private var rangeWidth : CGFloat = 1
+    
+    private var BarColor : Color {
+        get{
+            return (value >= CGFloat(Subject.MAX_MARK/2)) ? .green : .red
+        }
+    }
+    
+    init(mark : CGFloat = 10){
+        self.value = mark
+    }
+    
     var body: some View {
+        
         
         VStack{
             HStack{
@@ -20,26 +38,46 @@ struct UEView: View {
                         Text("6")
                     }
                     
-                    ZStack(alignment: .leading){
-                        Rectangle()
-                            .fill(.gray)
-                            .frame(height: 25)
-                            .cornerRadius(20)
-                        Rectangle()
-                            .fill(.green)
-                            .frame(height: 25)
-                            .frame(width: 50)
-                            .cornerRadius(20)
-                        
-                        HStack{
-                            Spacer()
-                            Text("5")
-                                .foregroundColor(.white)
-                            Spacer()
-                        }
-                        
+                    GeometryReader{geometry in
+                        ZStack(alignment: .leading){
+                            Rectangle()
+                                .fill(.gray)
+                                .frame(height: 25)
+                                .cornerRadius(20)
+                                .gesture(DragGesture()
+                                    .onChanged {
+                                        value in self.updateRange(value: value)
+                                    }
+                                )
+                            Rectangle()
+                                .fill(barColor)
+                                .frame(height: 25)
+                                .frame(width: pixelLen)
+                                .cornerRadius(20)
+                                .gesture(DragGesture()
+                                    .onChanged {
+                                        value in self.updateRange(value: value)
+                                    }
+                                )
+                                
                             
+                            HStack{
+                                Spacer()
+                                Text(String(format: "%.2f", value))
+                                    .foregroundColor(.white)
+                                Spacer()
+                            }
+                        }
+                        .onChange(of: geometry.size.width) { _ in
+                            updateRatio(width:geometry.size.width)
+                        }
+                        .onAppear(perform:{
+                            updateRatio(width:geometry.size.width)
+                        })
                     }
+                    .frame(height: 25)
+                    
+                    
                 }
                 
                 Image(systemName:"square.and.pencil")
@@ -54,10 +92,32 @@ struct UEView: View {
         }
         .padding(.top, 8)
     }
+
+    
+    private func updateRange(value: DragGesture.Value){
+        let dragLocation : CGFloat = value.location.x
+        let ratio = dragLocation / rangeWidth
+        let max = CGFloat(Subject.MAX_MARK)
+        let valPixel : CGFloat = dragLocation > 0 ? dragLocation : 0.0
+        let val : CGFloat = max * ratio
+        
+        self.pixelLen = min(valPixel,rangeWidth)
+        self.value = min(max, val > 0 ? val : 0.0)
+        self.barColor = BarColor
+    }
+    
+    private func updateRatio(width : CGFloat? = nil){
+        if let width=width{
+            rangeWidth = width
+        }
+        self.pixelLen = min(value*rangeWidth/CGFloat(Subject.MAX_MARK),rangeWidth)
+        self.barColor = BarColor
+    }
+    
 }
 
 struct UEView_Previews: PreviewProvider {
     static var previews: some View {
-        UEView()
+        UEView(mark:10)
     }
 }
